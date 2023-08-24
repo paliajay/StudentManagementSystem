4866c87e40fc48472b9c8b6f12036f54dabb1236
Ajay <ajaybabupali19@gmail.com>
2023-08-24 15:45:28 +0530

    Studentmanagment pro

---
+++ src/main/java/Student.java
@@ -0,0 +1,77 @@
+//step 2: create POJO (Plain Old Java Object) class -> contains constructors, getter, setter classes
+
+public class Student {
+    //student entity/domain class
+    //fields to be created: id, name, lastname, city, age
+    private int id;
+    private String name;
+    private String lastname;
+    private String city;
+    private int age;
+
+    //constructor with parameters
+    public Student(String name, String lastname, String city, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.city = city;
+        this.age = age;
+    }
+
+    //default constructor
+    public Student() {
+    }
+
+    //getter - setter -> to get id, name, lastname, city, age
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //to string method to print all info
+    @Override
+    public String toString() {
+        return
+                "id=" + id +
+                        ", name='" + name  +
+                        ", lastname='" + lastname +
+                        ", city='" + city +
+                        ", age=" + age;
+    }
+
+}
